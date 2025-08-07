5014eb0ee2477d28eb0c5af96079707465f3a074
Nicolás Torres <naturalezacomputacional@gmail.com>
2025-08-06 23:22:14 -0500

    Update server.R

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,26 +1,23 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+server <- function(input, output) {
+  datos <- reactive({
+    req(input$data)
+    read.csv(input$data$datapath)
   })
-  
-})
+
+  output$resumen <- renderPrint({
+    req(datos())
+    summary(datos())
+  })
+
+  output$grafico <- renderPlot({
+    req(datos())
+    plot(datos()[[1]], datos()[[2]], type = "l", main = "Ejemplo de gráfico")
+  })
+
+  output$plan <- renderTable({
+    data.frame(
+      Día = c("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"),
+      Actividad = c("Descanso", "Tenis", "32K Proyecto", "Descanso", "Entrenamiento", "Ciclismo", "Fondo")
+    )
+  })
+}
