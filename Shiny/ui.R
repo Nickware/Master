e44a8f67dee406d937dde01f10579575807ac7a8
Nicolás Torres <naturalezacomputacional@gmail.com>
2025-08-06 23:21:11 -0500

    Update ui.R

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,33 +1,21 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
+ui <- fluidPage(
+  titlePanel("Asistente de Entrenamiento y Nutrición"),
+
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      fileInput("data", "Cargar archivo de entrenamiento (CSV)", accept = ".csv"),
+      dateInput("fecha", "Selecciona fecha del entrenamiento"),
+      actionButton("procesar", "Procesar datos")
     ),
-    
-    # Show a plot of the generated distribution
+
     mainPanel(
-       plotOutput("distPlot")
+      tabsetPanel(
+        tabPanel("Resumen", verbatimTextOutput("resumen")),
+        tabPanel("Gráficos", plotOutput("grafico")),
+        tabPanel("Plan Semanal", tableOutput("plan"))
+      )
     )
   )
-))
+)
