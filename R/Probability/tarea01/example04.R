7e1ae130dc40682ce5244007750ea0a6a94d5e50
Nicolás Torres <naturalezacomputacional@gmail.com>
2025-08-25 22:34:52 -0500

    Restructuring examples with ggplot, dplyr using the iris dataset

--- R/Probability/tarea01/example04.R
+++
@@ -1,7 +0,0 @@
-# Package ggplot
-require(ggplot2)
-ggplot(data=iris, aes(x=Sepal.Length, y=Sepal.Width)) +  geom_point()
-myplot <-ggplot(data=iris, aes(x=Sepal.Length, y=Sepal.Width)) 
-myplot + geom_point()
-ggplot(data=iris, aes(x=Sepal.Length, y=Sepal.Width)) + geom_point(size=3)
-ggplot(data=iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species)) + geom_point(aes(shape=Species), size=3)
